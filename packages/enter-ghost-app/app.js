4ecabbc4fab0643122609d5b48d74d6b1132a8b5
Łukasz Lityński <hex13code@gmail.com>
2016-12-06 23:58:10 +0100

    enter-ghost-app

---
+++ packages/enter-ghost-app/app.js
@@ -0,0 +1,23 @@
+const EventEmitter =  require('events');
+
+const ghost = require('../vifi');
+
+module.exports = () => {
+    const app = new EventEmitter;
+    app.createEmitter = () => {
+        const ee = new EventEmitter
+        return ee;
+    };
+    app.vifi = ghost;
+
+    const variables = new Map;
+    app.get = (key) => {
+        return variables.get(key);
+    }
+    app.set = (key, data) => {
+        variables.set(key, data);
+        app.emit('set', key, data);
+    }
+
+    return app;
+};
