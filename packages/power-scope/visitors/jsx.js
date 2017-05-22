e596a924ed3599769dd1c49dcfa2dbebac4ec3ec
Łukasz Lityński <hex13code@gmail.com>
2017-05-22 17:46:47 +0200

    remove consolel.log

--- packages/power-scope/visitors/jsx.js
+++ packages/power-scope/visitors/jsx.js
@@ -2,22 +2,18 @@ let functions = [];
 module.exports = {
     FunctionDeclaration: {
         enter(node, state) {
-            console.log("KLAS METHOD")
             functions.push(state.nodeId);
         },
         exit(node, state) {
-            console.log("------KLAS METHOD")
             functions.pop();
         }
     },
 
     ClassMethod: {
         enter(node, state) {
-            console.log("KLAS METHOD")
             functions.push(state.nodeId);
         },
         exit(node, state) {
-            console.log("------KLAS METHOD")
             functions.pop();
         }
     },
@@ -35,9 +31,7 @@ module.exports = {
             const func = state.last('functions');
             if (func) {
                 let jsx = state.analysis.getComponent(func.nodeId, 'jsx');
-                console.log("POBOR JSX", func.nodeId, jsx);
                 if (!jsx) jsx= {uses: {}};
-                console.log("J S X", func.nodeId, jsx.uses, node.name);
                 if (node.name[0].toUpperCase() == node.name[0]) {
                     jsx.uses[node.name] = 1;
                 }
