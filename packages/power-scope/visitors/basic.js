b63da754a8c46e81ae576df5d5ce05e3b467e9d2
Łukasz Lityński <hex13code@gmail.com>
2017-06-01 18:53:42 +0200

    power-scope: extract visitor for refs

--- packages/power-scope/visitors/basic.js
+++ packages/power-scope/visitors/basic.js
@@ -28,7 +28,7 @@ const variableDeclarationVisitor = {
 }
 
 
-module.exports = {
+const basicVisitor = {
     ClassDeclaration: {
         exit(node, state) {
             const entity = {
@@ -145,106 +145,6 @@ module.exports = {
             }
 
         }
-    },
-    MemberExpression: {
-        exit(node, state) {
-            const chain = state.chains[state.chains.length - 1];
-            if (node.object.type == 'Identifier') {
-                chain.push({
-                    isChain: true,
-                    key: getName(node.object),
-                    scope: state.blockScopes[state.blockScopes.length - 1],
-                    loc: node.object.loc
-                });
-            }
-            if (node.object.type == 'ThisExpression') {
-                chain.push({
-                    isChain: true,
-                    key: 'this',
-                    scope: state.blockScopes[state.blockScopes.length - 1],
-                    loc: node.object.loc
-                });
-            }
-            chain.push({
-                key: '.'
-            });
-            chain.push({
-                key: getName(node.property),
-                loc: node.property.loc
-            })
-        },
-        enter(node, state) {
-
-        }
-    },
-    Identifier: {
-        enter(node, state) {
-        },
-        exit(node, state) {
-            const key = state.key;
-            if (key == 'expression' || key == 'arguments' || key == 'test' || key == 'left' || key == 'argument' || key == 'right' || key == 'init') {
-                state.declareRef([{
-                    isChain: true,
-                    key: getName(node),
-                    loc: node.loc,
-                    scope: state.blockScopes[state.blockScopes.length - 1]
-                }]);
-            }
-        }
-    },
-    ThisExpression: {
-        enter(node, state) {
-        },
-        exit(node, state) {
-            const key = state.key;
-            if (key == 'expression' || key == 'arguments' || key == 'test' || key == 'left' || key == 'argument' || key == 'right' || key == 'init') {
-                state.declareRef([{
-                    isChain: true,
-                    key: 'this',
-                    loc: node.loc,
-                    scope: state.blockScopes[state.blockScopes.length - 1]
-                }]);
-            }
-        }
-    },
-    CallExpression: {
-        enter(node, state) {
-            //state.ctx.push(null);
-            if (node.callee.type == 'Identifier') {
-                state.declareRef([{
-                    isChain: true,
-                    key: getName(node.callee),
-                    loc: node.callee.loc,
-                    scope: state.blockScopes[state.blockScopes.length - 1]
-                }]);
-
-            }
-        },
-        exit(node, state) {
-            if (node.callee.name == 'require' && node.arguments[0])
-                state.expr.push({
-                    origin: {
-                        name: '',
-                        path: node.arguments[0].value
-                    }
-                });
-            //state.ctx.pop();
-            // state.chains[state.chains.length - 1].push({
-            //     key: '()'
-            // });
-        }
-    },
-    ChainEnd: {
-        enter(node, state) {
-            const chain = [];
-            state.chains.push(chain);
-        },
-        exit(node, state) {
-            const ref = state.chains.pop();
-            if (ref.length) {
-                state.declareRef(ref);
-            }
-        },
     },
     ForStatement: {
         enter(node, state) {
@@ -408,3 +308,5 @@ module.exports = {
     }
 
 };
+
+module.exports = Object.assign(basicVisitor, require('./refs'));
