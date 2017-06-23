ad5f11c7a8d79c0c574e733a85337bc2de53967a
Łukasz Lityński <hex13code@gmail.com>
2017-06-23 22:01:00 +0200

    vifi: extract classes

--- packages/vifi/2.js
+++ packages/vifi/2.js
@@ -32,43 +32,63 @@ class File {
     }
 }
 
-function vfs(vfsToMount) {
-    const mainVfs = {
-        open(path) {
-            const file = new File(path);
-            file.connect(this);
-            return file;
-        },
-        read(file) {
-            return this._vfs.read(file);
-        },
-        write(file, data) {
-            return this._vfs.write(file, data);
-        },
-        mount(root, vfs) {
-
-            if (vfs.readFile) {
-                const fs = vfs;
-                vfs = {
-                    read(file) {
-                        return new Promise(resolve => {
-                            fs.readFile(file.path, 'utf8', (err, data) => {
-                                resolve(data);
-                            });
-                        });
-                    },
-                    write(file, data) {
-                        return new Promise(resolve => {
-                            fs.writeFile(file.path, data, 'utf8', () => {
-                                resolve();
-                            })
-                        });
-                    }
-                }
-            }
-            this._vfs = vfs;
+function wrapClass(Class) {
+    Object.getOwnPropertyNames(Class.prototype).forEach(prop => {
+        const originalMethod = Class.prototype[prop];
+        Class.prototype[prop] = function (...args) {
+            console.log(`${Class.name}::${prop}`, args)
+            return originalMethod.apply(this, args);
         }
-    };
+    });
+}
+
+wrapClass(File);
+
+class NodeFsWrapper {
+    constructor(fs) {
+        this._fs = fs;
+    }
+    inspect() {
+        return '[NodeFsWrapper]'
+    }
+    read(file) {
+        return new Promise(resolve => {
+            this._fs.readFile(file.path, 'utf8', (err, data) => {
+                resolve(data);
+            });
+        });
+    }
+    write(file, data) {
+        return new Promise(resolve => {
+            this._fs.writeFile(file.path, data, 'utf8', () => {
+                resolve();
+            })
+        });
+    }
+}
+
+class MainFileSystem {
+    open(path) {
+        const file = new File(path);
+        file.connect(this);
+        return file;
+    }
+    read(file) {
+        return this._vfs.read(file);
+    }
+    write(file, data) {
+        return this._vfs.write(file, data);
+    }
+    mount(root, vfs) {
+        if (vfs.readFile) {
+            vfs = new NodeFsWrapper(vfs);
+        }
+        this._vfs = vfs;
+    }
+}
+
+function vfs(vfsToMount) {
+    const mainVfs = new MainFileSystem;
     if (vfsToMount) {
         mainVfs.mount('/', vfsToMount);
     }
