d75f1e27b42fe465bce40a65a8af7be7ab588a30
Łukasz Lityński <hex13code@gmail.com>
2017-06-27 18:50:00 +0200

    oop-as-text: register constructors

--- packages/oop-as-text/index.js
+++ packages/oop-as-text/index.js
@@ -1,8 +1,20 @@
+const classes = Object.create(null);
+
+exports.register = function (name, constructor) {
+    classes[name] = constructor;
+}
 exports.serialize = function (obj) {
     return JSON.stringify(obj);
 }
+
+function reviver(key, value) {
+    if (value.type) {
+        return new classes[value.type](value);
+    }
+    return value;
+};
+
 exports.deserialize = function (string) {
-    return JSON.parse(string);
+
+    return JSON.parse(string, reviver);
 }
-
-
--- packages/oop-as-text/test/oopAsTextSpec.js
+++ packages/oop-as-text/test/oopAsTextSpec.js
@@ -1,12 +1,39 @@
 const oopAsText = require('..');
 const assert = require('assert');
 
-describe('', () => {
-    it('should serialize', () => {
-        const json = oopAsText.serialize({a: 3});
+
+class A {
+    constructor({a}) {
+        this.a = a;
+    }
+    sum(a, b) {
+        return a + b;
+    }
+    toJSON() {
+        return {
+            type: 'A',
+            a: this.a
+        };
+    }
+}
+
+class B {
+}
+
+describe('oop-as-text', () => {
+    it('should serialize and deserialize', () => {
+        oopAsText.register('A', A);
+        oopAsText.register('B', B);
+
+        const obj = new A({a: 3});
+        const json = oopAsText.serialize(obj);
+
         assert.equal(Object.prototype.toString.call(json),'[object String]');
+
         const rehydrated = oopAsText.deserialize(json);
+
+        assert(rehydrated instanceof A);
         assert.deepEqual(rehydrated, {a: 3});
+        assert.equal(rehydrated.sum(10, 20), 30);
     });
 });
-
