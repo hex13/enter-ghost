ecbf37cf02a53601c8e10a141d5ccc7c75aec740
Łukasz Lityński <hex13code@gmail.com>
2017-06-27 21:59:00 +0200

    oop-as-text: test nesting

--- packages/oop-as-text/test/oopAsTextSpec.js
+++ packages/oop-as-text/test/oopAsTextSpec.js
@@ -3,8 +3,9 @@ const assert = require('assert');
 
 
 class A {
-    constructor({a}) {
+    constructor({a, b}) {
         this.a = a;
+        this.b = b;
     }
     sum(a, b) {
         return a + b;
@@ -12,12 +13,22 @@ class A {
     toJSON() {
         return {
             type: 'A',
-            a: this.a
+            a: this.a,
+            b: this.b
         };
     }
 }
 
 class B {
+    constructor({v} = {}) {
+        this.v = v;
+    }
+    toJSON() {
+        return {
+            type: 'B',
+            v: this.v
+        }
+    }
 }
 
 describe('oop-as-text', () => {
@@ -25,15 +36,15 @@ describe('oop-as-text', () => {
         oopAsText.register('A', A);
         oopAsText.register('B', B);
 
-        const obj = new A({a: 3});
+        const obj = new A({a: 3, b: new B({v:10})});
         const json = oopAsText.serialize(obj);
 
         assert.equal(Object.prototype.toString.call(json),'[object String]');
 
         const rehydrated = oopAsText.deserialize(json);
-
-        assert(rehydrated instanceof A);
-        assert.deepEqual(rehydrated, {a: 3});
+        assert(rehydrated !== obj && (rehydrated instanceof A));
+        assert(rehydrated.b !== obj.b && (rehydrated.b instanceof B));
+        assert.deepEqual(rehydrated, {a: 3, b:{v:10}});
         assert.equal(rehydrated.sum(10, 20), 30);
     });
 });
