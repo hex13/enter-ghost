f206d124642adc7754e91519dadd41a01a289108
Łukasz Lityński <hex13code@gmail.com>
2017-07-07 15:29:24 +0200

    vistate: extract Transaction to separate file

--- packages/vistate/stateContainer.js
+++ packages/vistate/stateContainer.js
@@ -16,48 +16,7 @@ function createEvent(model, type, args) {
     return {target: model.$localId(), type, args};
 }
 
-class Transaction {
-    constructor(handlers = {}) {
-        this.onEnd = handlers.onEnd;
-        this.onCommit = handlers.onCommit;
-        for (let eventName in handlers) {
-            if (
-                eventName.slice(0, 2) == 'on'
-                && eventName != 'onEnd'
-                && eventName != 'onCommit'
-                && eventName != 'onInit'
-            ) {
-                const methName = eventName.charAt(2).toLowerCase() + eventName.slice(3);
-                this[methName] = () => {
-                    handlers[eventName](this);
-                }
-            }
-        }
-        this._tasks = [];
-        this._data = Object.create(null);
-
-        handlers.onInit && handlers.onInit(this);
-    }
-    data(k, v) {
-        if (k === undefined) return this._data;
-        if (v === undefined) return this._data[k];
-        this._data[k] = v;
-    }
-    task(cb) {
-        this._tasks.push(cb);
-    }
-    // cancel() {
-    //     this.onEnd && this.onEnd(this);
-    // }
-    commit() {
-        this._tasks.forEach(task => task());
-        this.onCommit && this.onCommit(this);
-        this.onEnd && this.onEnd(this);
-    }
-    end(resultState) {
-        this.onEnd && this.onEnd(resultState);
-    }
-}
+const Transaction = require('./transaction');
 
 
 const ROOT_LOCAL_ID = 1234;
