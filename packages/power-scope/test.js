98c42112dbf55948f505a74d836562b06b75346a
Łukasz Lityński <hex13code@gmail.com>
2017-07-16 14:40:03 +0200

    power-scope: test.js

--- packages/power-scope/test.js
+++ packages/power-scope/test.js
@@ -1,7 +1,19 @@
 "use strict";
-
+process.send = process.send || console.log;
 const mocha = new (require('mocha'));
-mocha.addFile('test/psSpec.js');
-mocha.run(failures => {
-    console.log("FAILURES", failures)
+mocha.addFile(__dirname + '/test/6spec.js');
+
+let messages = [];
+const runner = mocha.run(failures => {
+    console.error("FAILURES", failures)
+    process.send({
+        failures,
+        messages
+    })
+    process.exit();
 });
+
+runner.on('fail', (err) => {
+    console.log("FAIL")
+    messages = [err.err.stack];
+})
