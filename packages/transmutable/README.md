8cb09d36acc4ccf6ebc24e453c8712fb8ab0f6ad
Łukasz Lityński <hex13code@gmail.com>
2017-08-02 23:12:59 +0200

    transmutable: readme, package

--- packages/transmutable/README.md
+++ packages/transmutable/README.md
@@ -3,9 +3,10 @@
 Example:
 
 ```javascript
-
-const { Transmutable } = require('transmutable');
 const log = console.log.bind(console);
+
+const { Transmutable, transform } = require('transmutable');
+
 
 const original = {
     cow: 123,
@@ -14,17 +15,30 @@ const original = {
     }
 };
 
+// easy way:
+
+const copy = transform(original, stage => {
+    stage.cow = 'doge';
+});
+
+        log(copy); // { cow: 'doge', dogs: { muchWow: 1 } }
+        log(original); // still the same: { cow: 123, dogs: { muchWow: 1 } }
+        log(copy.dogs === original.dogs); // true
+
+
+// or using more detailed API. We create reusable Transmutable object
+
 const t = new Transmutable(original);
 t.stage.cow = 456;
 t.stage.dogs.muchWow = 888888;
-    log(t.reify()); // { cow: 456, dogs: { muchWow: 888888 } }
-    log(t.stage.dogs); // { muchWow: 1 }
+
+        log(t.reify()); // { cow: 456, dogs: { muchWow: 888888 } }
+        log(t.stage.dogs); // { muchWow: 1 }
 
 const copied = t.commit();
 
-    log(copied); // { cow: 456, dogs: { muchWow: 888888 } }
-
-    log(original); // { cow: 123, dogs: { muchWow: 1 } }
+        log(copied); // { cow: 456, dogs: { muchWow: 888888 } }
+        log(original); // { cow: 123, dogs: { muchWow: 1 } }
 
 ```
 
--- packages/transmutable/test/example.js
+++ packages/transmutable/test/example.js
@@ -8,9 +8,10 @@ describe('example', () => {
         };
 
         // COPY FROM HERE
-
-        const { Transmutable } = require('transmutable');
         const log = console.log.bind(console);
+
+        const { Transmutable, transform } = require('transmutable');
+
 
         const original = {
             cow: 123,
@@ -19,17 +20,30 @@ describe('example', () => {
             }
         };
 
+        // easy way:
+
+        const copy = transform(original, stage => {
+            stage.cow = 'doge';
+        });
+
+                log(copy); // { cow: 'doge', dogs: { muchWow: 1 } }
+                log(original); // still the same: { cow: 123, dogs: { muchWow: 1 } }
+                log(copy.dogs === original.dogs); // true
+
+
+        // or using more detailed API. We create reusable Transmutable object
+
         const t = new Transmutable(original);
         t.stage.cow = 456;
         t.stage.dogs.muchWow = 888888;
-            log(t.reify()); // { cow: 456, dogs: { muchWow: 888888 } }
-            log(t.stage.dogs); // { muchWow: 1 }
+
+                log(t.reify()); // { cow: 456, dogs: { muchWow: 888888 } }
+                log(t.stage.dogs); // { muchWow: 1 }
 
         const copied = t.commit();
 
-            log(copied); // { cow: 456, dogs: { muchWow: 888888 } }
-
-            log(original); // { cow: 123, dogs: { muchWow: 1 } }
+                log(copied); // { cow: 456, dogs: { muchWow: 888888 } }
+                log(original); // { cow: 123, dogs: { muchWow: 1 } }
 
         // END COPY FROM HERE
 
