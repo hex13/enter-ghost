f2e4b940d9dbab749bc0413bccc46795c562d440
Łukasz Lityński <hex13code@gmail.com>
2017-08-03 12:25:56 +0200

    vistate: refactor

--- packages/vistate/stateContainer.js
+++ packages/vistate/stateContainer.js
@@ -104,189 +104,191 @@ const generateId = (last => () => ++last)(0);
 
 
 
-const init = () => ({
-    transaction(model, callback, tempState) {
-        const transaction = new Transaction({
-            onEnd: resultState => {
-                Object.assign(model.getEntity().state, savedState);
-                Object.assign(model.getEntity().state, resultState);
-            }
-        });
-        let savedState = {};
-        Object.keys(tempState).forEach(k => {
-            savedState[k] = model.getEntity().state[k];
-        });
-        Object.assign(model.getEntity().state, tempState); // TODO extract as action
-        return callback(transaction, model);
-    },
-    systems: middleware,
-    runningSystems: {
-
-    },
-    defaultSystems: [
-        'runHandlerAndNotify',
-        'record',
-        'notifier'
-    ],
-    //systemInstances: Object.create(null),
-    dbg(model) {
-        return JSON.stringify(model.get());
-    },
-    events(model) {
-        return this.component(this.root(model), 'events').filter(event => {
-            return this.root(model) == model || event.target == model.$localId();
-        });
-    },
-    component(model, id, value) {
-        const componentsById = model._componentsById;
-        if (value !== undefined) {
-            componentsById[id] = value;
-        }
-        return componentsById[id];
-    },
-    system(name) {
-        if (this.runningSystems.hasOwnProperty(name)) {
-            return this.runningSystems[name];
-        }
-        const id = Symbol(name);
-        if (this.systems.hasOwnProperty(name)) {
-            const system = {
-                system: this.systems[name](id),
-                id
-            };
-            this.runningSystems[name] = system;
-            return system;
-        }
-    },
-    root(model) {
-        return model._root;
-    },
-    reset(model) {
-        model.$reset();
-    },
-    autocorrect(model, methodName) {
-        const props = Object.keys(model);
-        return correct(methodName, props);
-    },
-    dispatch(model, {target, type, args}) {
-        if (target && target != ROOT_LOCAL_ID ) {
-            model = model.getEntity().find(target);
-        }
-        model[type](...args);
-    },
-    undo(model) {
-        model.getEntity().dispatch({model, name: '$undo'})
-    },
-    model(blueprint, params = {}) {
-
-        const model = {};
-
-        model._root = model;
-
-        model._componentsById = Object.create(null);
-
-        const componentRefs = this.defaultSystems.map(name => {
-            const { system, id } =  this.system(name);
-            return {
-                system,
-                data: this.component(model, id, {
-                    of: (model) => this.component(model, id),
-                })
-            };
-        });
-
-        const modelApi = {};
-
-        const queries = {
-            get: getProperty
-        };
-
-        if (blueprint.queries) {
-            Object.assign(queries, blueprint.queries);
-        }
-
-        for (let name in queries) {
-            model[name] = (...args) => {
-                return queries[name](model.getEntity().state, ...args);
-            }
-        }
-
-        const entity = new Entity(blueprint, {
-            componentRefs, data: blueprint.data, api: this, model
-        });
-
-        model._localId = entity._localId;
-
-        let methods = Object.keys(blueprint.actions||{}).concat('$subscribe', 'set');
-
-        const actions = blueprint.actions || {};
-
-        methods.forEach(name => {
-            const original = actions[name] || standardActions[name];
-            if (params.use) {
-                componentRefs.push.apply(componentRefs, params.use.map(system => {
-                        return {system: this.system(system).system};
-                }));
-            }
-
-            model[name] = (...args) => {
-                const actionData = { original, value: undefined, model, name, args, payload: args[0] };
-                entity.dispatch(actionData);
-                return actionData.value;
-            }
-        });
-
-        model.getEntity = () => entity;
-
-        model.$localId = () => model._localId;
-        model.getId = () => model._localId;
-
-        _connectChildren(model._root, model, entity.state);
-        model._metadata = {type: blueprint.type};
-        // TODO remove it
-        model.valueOf = () => model.constructor.name;
-        return model;
-    },
-    metadata(model, md) {
-        if (md) {
-            metadata.set(model, md);
-            return
-        }
-        return model._metadata || metadata.get(model);
-    },
-    collection() {
-        return this.model({
-            data: {
-                list: []
-            },
-            queries: {
-                get(state) {
-                    return state.list;
+const init = () => {
+    return {
+        transaction(model, callback, tempState) {
+            const transaction = new Transaction({
+                onEnd: resultState => {
+                    Object.assign(model.getEntity().state, savedState);
+                    Object.assign(model.getEntity().state, resultState);
                 }
-            },
-            actions: {
-                add(state, item) {
-                    state.list.push(item);
-                    // TODO connect list item
-                    if (isModel(item)) {
-                        item._root = this;
-                        item._localId = this.getEntity().register(item);
-                    }
+            });
+            let savedState = {};
+            Object.keys(tempState).forEach(k => {
+                savedState[k] = model.getEntity().state[k];
+            });
+            Object.assign(model.getEntity().state, tempState); // TODO extract as action
+            return callback(transaction, model);
+        },
+        systems: middleware,
+        runningSystems: {
 
+        },
+        defaultSystems: [
+            'runHandlerAndNotify',
+            'record',
+            'notifier'
+        ],
+        //systemInstances: Object.create(null),
+        dbg(model) {
+            return JSON.stringify(model.get());
+        },
+        events(model) {
+            return this.component(this.root(model), 'events').filter(event => {
+                return this.root(model) == model || event.target == model.$localId();
+            });
+        },
+        component(model, id, value) {
+            const componentsById = model._componentsById;
+            if (value !== undefined) {
+                componentsById[id] = value;
+            }
+            return componentsById[id];
+        },
+        system(name) {
+            if (this.runningSystems.hasOwnProperty(name)) {
+                return this.runningSystems[name];
+            }
+            const id = Symbol(name);
+            if (this.systems.hasOwnProperty(name)) {
+                const system = {
+                    system: this.systems[name](id),
+                    id
+                };
+                this.runningSystems[name] = system;
+                return system;
+            }
+        },
+        root(model) {
+            return model._root;
+        },
+        reset(model) {
+            model.$reset();
+        },
+        autocorrect(model, methodName) {
+            const props = Object.keys(model);
+            return correct(methodName, props);
+        },
+        dispatch(model, {target, type, args}) {
+            if (target && target != ROOT_LOCAL_ID ) {
+                model = model.getEntity().find(target);
+            }
+            model[type](...args);
+        },
+        undo(model) {
+            model.getEntity().dispatch({model, name: '$undo'})
+        },
+        model(blueprint, params = {}) {
+
+            const model = {};
+
+            model._root = model;
+
+            model._componentsById = Object.create(null);
+
+            const componentRefs = this.defaultSystems.map(name => {
+                const { system, id } =  this.system(name);
+                return {
+                    system,
+                    data: this.component(model, id, {
+                        of: (model) => this.component(model, id),
+                    })
+                };
+            });
+
+            const modelApi = {};
+
+            const queries = {
+                get: getProperty
+            };
+
+            if (blueprint.queries) {
+                Object.assign(queries, blueprint.queries);
+            }
+
+            for (let name in queries) {
+                model[name] = (...args) => {
+                    return queries[name](model.getEntity().state, ...args);
+                }
+            }
+
+            const entity = new Entity(blueprint, {
+                componentRefs, data: blueprint.data, api: this, model
+            });
+
+            model._localId = entity._localId;
+
+            let methods = Object.keys(blueprint.actions||{}).concat('$subscribe', 'set');
+
+            const actions = blueprint.actions || {};
+
+            methods.forEach(name => {
+                const original = actions[name] || standardActions[name];
+                if (params.use) {
+                    componentRefs.push.apply(componentRefs, params.use.map(system => {
+                            return {system: this.system(system).system};
+                    }));
+                }
+
+                model[name] = (...args) => {
+                    const actionData = { original, value: undefined, model, name, args, payload: args[0] };
+                    entity.dispatch(actionData);
+                    return actionData.value;
+                }
+            });
+
+            model.getEntity = () => entity;
+
+            model.$localId = () => model._localId;
+            model.getId = () => model._localId;
+
+            _connectChildren(model._root, model, entity.state);
+            model._metadata = {type: blueprint.type};
+            // TODO remove it
+            model.valueOf = () => model.constructor.name;
+            return model;
+        },
+        metadata(model, md) {
+            if (md) {
+                metadata.set(model, md);
+                return
+            }
+            return model._metadata || metadata.get(model);
+        },
+        collection() {
+            return this.model({
+                data: {
+                    list: []
                 },
-            }
-        });
-    },
-    delegateTo(childName, methodName) {
-        return (state) => {
-            if (state.hasOwnProperty(childName)) {
-                const model = state[childName];
-                console.log("######TG", model[methodName]());
-            }
+                queries: {
+                    get(state) {
+                        return state.list;
+                    }
+                },
+                actions: {
+                    add(state, item) {
+                        state.list.push(item);
+                        // TODO connect list item
+                        if (isModel(item)) {
+                            item._root = this;
+                            item._localId = this.getEntity().register(item);
+                        }
 
+                    },
+                }
+            });
+        },
+        delegateTo(childName, methodName) {
+            return (state) => {
+                if (state.hasOwnProperty(childName)) {
+                    const model = state[childName];
+                    console.log("######TG", model[methodName]());
+                }
+
+            }
         }
     }
-});
+}
 
 module.exports = {
     Transaction,
