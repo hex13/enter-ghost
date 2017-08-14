bb05a54761cf890d37d715723622116c082c5781
Łukasz Lityński <hex13code@gmail.com>
2017-08-15 00:36:22 +0200

    serialeasy: array support

--- packages/serialeasy/index.js
+++ packages/serialeasy/index.js
@@ -1,21 +1,32 @@
 function serialize(obj) {
-    return Object
-        .keys(obj)
-        .sort((a, b) => a < b? -1 : 1)
-        .map(k => {
-            let v = obj[k];
-            if (v && typeof v == 'object') v = serialize(v);
-            return [k, v];
-        });
+    if (Array.isArray(obj)) {
+        return ["arr", obj.map(serialize)]
+    }
+    if (obj && typeof obj == 'object') {
+        return Object
+            .keys(obj)
+            .sort((a, b) => a < b? -1 : 1)
+            .map(k => [k, serialize(obj[k])]);
+    }
+    return obj;
 };
 
 function deserialize(data) {
-    const obj = {};
-    data.forEach(([k, v, t]) => {
-        if (Array.isArray(v)) v = deserialize(v);
-        obj[k] = v;
-    });
-    return obj;
+    if (!Array.isArray(data)) return data;
+
+    if (Object.prototype.toString.call(data[0]) == '[object String]') {
+        return data[1].map(deserialize);
+    }
+
+    if (data && typeof data == 'object') {
+        const obj = {};
+        data.forEach(([k, v, t]) => {
+            if (Array.isArray(v)) v = deserialize(v);
+            obj[k] = v;
+        });
+        return obj;
+    }
+
 }
 
 exports.serialize = serialize;
--- packages/serialeasy/test/serialeasySpec.js
+++ packages/serialeasy/test/serialeasySpec.js
@@ -1,88 +1,137 @@
 const { serialize, deserialize } = require('..');
 const assert = require('assert');
 
-const createExampleA = () => ({
-    a: 2
-});
-
 describe('Serialeasy', () => {
-    describe('serializes object', () => {
-        it('with number property', () => {
-            const o = {a: 2};
-            assert.deepStrictEqual(serialize(o), [
-                ["a", 2]
-            ])
+    describe('serialization', () => {
+        it('of number', () => {
+            assert.strictEqual(serialize(30), 30);
         });
-        it('with string property', () => {
-            const o = {s: "Hello"};
-            assert.deepStrictEqual(serialize(o), [
-                ["s", "Hello"]
-            ])
+        it('of string', () => {
+            assert.strictEqual(serialize('squirrel'), 'squirrel');
         });
-        it('with multiple keys (alphabetic sorting)', () => {
-            const o = {s: "Hello", a: 99, n: 20};
-            assert.deepStrictEqual(serialize(o), [
-                ["a", 99],
-                ["n", 20],
-                ["s", "Hello"],
-            ])
-        });
-        it('with nesting', () => {
-            const o = {
-                prop: 123,
-                subObject: {
-                    a: 1,
-                    c: "three",
-                    b: 2,
-                }
-            };
-            assert.deepStrictEqual(serialize(o), [
-                ["prop", 123],
-                [
-                    "subObject",
-                    [
-                        ["a", 1],
-                        ["b", 2],
-                        ["c", "three"],
+        describe('of array', () => {
+            it('with numbers and strings', () => {
+                assert.deepStrictEqual(serialize([1, 2, 4, "eight"]), ["arr", [1, 2, 4, "eight"]]);
+            });
+            it('nested arrays', () => {
+                assert.deepStrictEqual(serialize([1, 2, 4, [16, 32]]), [
+                    "arr", [
+                        1, 2, 4, [
+                            "arr",
+                            [16, 32]
+                        ]
                     ]
-                ],
-            ]);
+                ]);
+            });
+            it('with object as an item', () => {
+                assert.deepStrictEqual(
+                    serialize([100, {monkey: 'banana'}]),
+                    ["arr", [
+                        100, [["monkey", "banana"]]
+                    ]]
+                )
+            });
+        });
+
+        describe('serializes object', () => {
+            it('with array property', () => {
+                const o = {numbers: [1, 10, 100, "thousand"]};
+                assert.deepStrictEqual(serialize(o), [
+                    ["numbers", ["arr", [1, 10,100, "thousand"]]]
+                ]);
+            });
+            it('with multiple keys (alphabetic sorting)', () => {
+                const o = {s: "Hello", a: 99, n: 20};
+                assert.deepStrictEqual(serialize(o), [
+                    ["a", 99],
+                    ["n", 20],
+                    ["s", "Hello"],
+                ])
+            });
+            it('with nesting', () => {
+                const o = {
+                    prop: 123,
+                    subObject: {
+                        a: 1,
+                        c: "three",
+                        b: 2,
+                    }
+                };
+                assert.deepStrictEqual(serialize(o), [
+                    ["prop", 123],
+                    [
+                        "subObject",
+                        [
+                            ["a", 1],
+                            ["b", 2],
+                            ["c", "three"],
+                        ]
+                    ],
+                ]);
+            });
         });
     });
 
-    describe('deserializes object', () => {
-        it('with number property', () => {
-            const data = [["a", 81]];
-            assert.deepStrictEqual(deserialize(data), {a: 81});
+
+    describe('deserialization', () => {
+        it('of number', () => {
+            assert.strictEqual(deserialize(3), 3);
         });
-        it('with string property', () => {
-            const data = [["b", "tekst"]];
-            assert.deepStrictEqual(deserialize(data), {b: 'tekst'});
+        it('of string', () => {
+            assert.strictEqual(deserialize('house'), 'house');
         });
-        it('with multiple keys', () => {
-            const data = [["a", "tekst"], ["b", 3]];
-            assert.deepStrictEqual(deserialize(data), {
-                a: "tekst",
-                b: 3
-            });
+        it('of array', () => {
+            assert.deepStrictEqual(deserialize(["arr", [90, 89]]), [90, 89]);
         });
-        it('with nesting', () => {
-            const data = [
-                ["o", 2],
-                [
-                    "sub",
-                    [
-                        ["p", 2],
-                        ["r", "abc"]
+        it('of nested array', () => {
+            assert.deepStrictEqual(deserialize([
+                "arr", [
+                    90, 89, [
+                        "arr", [
+                            88, 87, [
+                                "arr", [86, 85, 84]
+                            ]
+                        ]
                     ]
                 ]
-            ];
-            assert.deepStrictEqual(deserialize(data), {
-                o: 2,
-                sub: {
-                    p: 2,
-                    r: "abc"
-                }
+            ]), [90, 89, [88, 87, [86, 85, 84]]]);
+        });
+        it('of array with object as an item', () => {
+            assert.deepStrictEqual(deserialize(
+                [
+                    "arr", [
+                        9, [["tree", "elm"]]
+                    ]
+                ]
+            ), [9, {tree: 'elm'}]);
+        });
+
+        describe('deserializes object', () => {
+            it('with multiple keys', () => {
+                const data = [["a", "tekst"], ["b", 3]];
+                assert.deepStrictEqual(deserialize(data), {
+                    a: "tekst",
+                    b: 3
+                });
+            });
+            it('with nesting', () => {
+                const data = [
+                    ["o", 2],
+                    [
+                        "sub",
+                        [
+                            ["p", 2],
+                            ["r", "abc"]
+                        ]
+                    ]
+                ];
+                assert.deepStrictEqual(deserialize(data), {
+                    o: 2,
+                    sub: {
+                        p: 2,
+                        r: "abc"
+                    }
+                });
             });
         });
     });
@@ -93,7 +142,8 @@ describe('Serialeasy', () => {
             b: "a",
             c: {
                 d: {
-                    e: 120
+                    e: 120,
+                    f: [10, 20, [1, {f: 2}]]
                 }
             }
         };
