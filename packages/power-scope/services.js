69069719bf44f14b21b22a6d603123294dcf35ee
Łukasz Lityński <hex13code@gmail.com>
2017-11-22 15:14:49 +0100

    power-scope: move postprocess to services

--- packages/power-scope/services.js
+++ packages/power-scope/services.js
@@ -77,7 +77,56 @@ module.exports = (getters) => {
         return entity;
     }
 
+
+    function postprocess(state, services) {
+
+        const analysis = state.analysis;
+        services.forEachRef(state, ref_ => {
+            const ref = ref_.slice();
+            let baseVariable = services.resolveRef([ref[0]]);
+            if (!baseVariable) {
+                // declare implicit global variable
+                state.declareVariable({
+                    name: ref[0].key,
+                    scope: analysis.scopes[0],
+                    loc: {start:{column:0,line:1}, end:{line:1, column:0}},
+                    isImplicit: true,
+                    refs: [ref.slice()],
+                });
+            }
+            while (ref.length) {
+                const entity = services.resolveRef(ref);
+                if (entity) {
+                    entity.refs = entity.refs || [];
+                    entity.refs.push(ref.slice());
+                } else if(baseVariable) {
+                    // declare implicit entry
+                    baseVariable.scope.entries[services.textOf(ref)] = {
+                        name: ref[ref.length - 1].name,
+                        scope: baseVariable.scope,
+                        loc: {start:{column:0,line:1}, end:{line:1, column:0}},
+                        isImplicit: true,
+                        refs: [ref.slice()],
+                    };
+                }
+                ref.pop();
+                if (ref.length && ref[ref.length - 1].key == '.') ref.pop();
+            }
+        });
+
+        // set components for module.exports
+        if (analysis.scopes[0]) {
+            Object.keys(analysis.scopes[0].entries)
+                .filter(k => k.indexOf('module.exports.') == 0)
+                .map(k => analysis.scopes[0].entries[k])
+                .forEach((entry) => {
+                    analysis.setComponent('file', 'exports.' + entry.name, entry);
+                });
+        }
+
+    }
+
     return {
-        lookupEntry, resolveRef, refAt, scopeAt, entryAt
+        lookupEntry, resolveRef, refAt, scopeAt, entryAt, postprocess
     };
 };
