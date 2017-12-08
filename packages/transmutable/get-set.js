fe29feb4d2951ddbb2e92c5d94290ccfc78e6ffa
Łukasz Lityński <hex13code@gmail.com>
2017-12-08 22:26:57 +0100

    transmutable: extract get, set

---
+++ packages/transmutable/get-set.js
@@ -0,0 +1,11 @@
+exports.get = function get(target, path) {
+  let curr = target, i;
+  for (i = 0; curr && i < path.length - 1; curr = curr[path[i++]]) ;
+  if (curr) return curr[path[i]];
+}
+exports.set = function set(target, path, value) {
+  let curr = target, i;
+  for (i = 0; i < path.length - 1; curr = curr[path[i++]]) ;
+  curr[path[i]] = value
+}
+
--- packages/transmutable/transmutable.js
+++ packages/transmutable/transmutable.js
@@ -1,17 +1,6 @@
 "use strict";
 
-
-function set(target, path, value) {
-  let curr = target, i;
-  for (i = 0; i < path.length - 1; curr = curr[path[i++]]) ;
-  curr[path[i]] = value
-}
-
-function get(target, path) {
-  let curr = target, i;
-  for (i = 0; curr && i < path.length - 1; curr = curr[path[i++]]) ;
-  if (curr) return curr[path[i]];
-}
+const { set, get } = require('./get-set');
 
 function isDirty(mutations, propPath, target) {
     for (let i = 0; i < mutations.length; i++) {
