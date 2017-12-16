a8abba38dc3e7af3b24ee65b43e70422793c1553
Łukasz Lityński <hex13code@gmail.com>
2017-12-16 13:39:32 +0100

    transmutable: decouple createStage 1

--- packages/transmutable/transmutable.js
+++ packages/transmutable/transmutable.js
@@ -66,34 +66,41 @@ function Transmutable(o) {
     this.commits = [];
     this.lastCommit = new Commit();
 
-    const createStage = (o, path = []) => {
-        const getTarget = () => typeof o == 'function'? o(): o;
-        const proxy = new Proxy(getTarget(), {
-            get: (nonUsedProxyTarget, name) => {
-                // transmutable.target can change
-                // so we want to have always the current target
-                const target = getTarget();
 
-                if (target[name] && typeof target[name] == 'object') {
-                    return createStage(target[name], path.concat(name));
+    const createStage = (target, handlers) => {
+        const _createStage = (o, path = []) => {
+            const getTarget = () => typeof o == 'function'? o(): o;
+            const proxy = new Proxy(getTarget(), {
+                get: (nonUsedProxyTarget, name) => {
+                    // transmutable.target can change
+                    // so we want to have always the current target
+                    const target = getTarget();
+
+                    if (target[name] && typeof target[name] == 'object') {
+                        return _createStage(target[name], path.concat(name));
+                    }
+                    return target[name];
+                },
+                set: (nonUsedProxyTarget, k, v) => {
+                    const mutPath = [];
+                    for (let i = 0; i < path.length + 1; i++) {
+                        mutPath.push(path[i] || k)
+                    }
+                    handlers.set(mutPath, v);
+                    return true;
                 }
-                return target[name];
-            },
-            set: (nonUsedProxyTarget, k, v) => {
-                const mutPath = [];
-                for (let i = 0; i < path.length + 1; i++) {
-                    mutPath.push(path[i] || k)
-                }
-                this.mutations.push([mutPath, v])
-                // TODO something like that (decouple recording logic)
-                // onSet(target, mutpath, v);
-                return true;
-            }
-        });
-        return proxy;
+            });
+            return proxy;
+        }
+
+        return _createStage(target);
     }
 
-    this.stage = createStage(() => this.target);
+    this.stage = createStage(() => this.target, {
+        set: (path, v) => {
+            this.mutations.push([path, v])
+        }
+    });
 }
 
 // TODO
