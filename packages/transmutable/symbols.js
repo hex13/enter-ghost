6dc0e0accf830b25a3ca261144570f53d6300f99
Łukasz Lityński <hex13code@gmail.com>
2018-01-17 20:11:04 +0100

    transmutable: symbols

--- packages/transmutable/symbols.js
+++ packages/transmutable/symbols.js
@@ -8,4 +8,5 @@ const METHOD = Symbol();
 const MUTATION = Symbol();
 const WAS_ACCESSED = Symbol();
 const DRAFT = Symbol();
-module.exports = { WAS_ACCESSED, WAS_WRITTEN, METHOD, MUTATION, DRAFT };
+const SELECTOR = Symbol();
+module.exports = { WAS_ACCESSED, WAS_WRITTEN, METHOD, MUTATION, DRAFT, SELECTOR };
