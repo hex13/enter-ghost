f18ded71e172b93b74e98093e25fca6d4bd1504e
Łukasz Lityński <hex13code@gmail.com>
2018-02-05 23:01:51 +0100

    remove transmutable (it is moved to separate repo: https://github.com/hex13/transmutable #10

--- packages/transmutable/scripts/buildDocs.js
+++
@@ -1,8 +0,0 @@
-const H = require('handlebars');
-const fs = require('fs');
-
-const tpl = fs.readFileSync(__dirname + '/../' + 'README.md.tpl', 'utf8');
-const data = {
-    size: (fs.statSync(__dirname + '/../dist/transmutable.js').size / 1024).toFixed(2)
-};
-console.log(H.compile(tpl)(data));
